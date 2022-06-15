4c1e4968e229685b310165ff18f29f17478b0d10
bipnrai054 <bipinrai054@gmail.com>
2022-06-15 16:52:47 +0545

    intor done

---
+++ screens/Details.js
@@ -0,0 +1,13 @@
+import {View,Text} from 'react-native'
+
+const Details = () => {
+  return (
+    <View>
+      <Text>
+        Details
+      </Text>
+    </View>
+  )
+}
+
+export default Details
